1700c910354c12969ae88fee10fd04f164eeddff
Gianmarco-26 <gianmarcolorusso26@gmail.com>
2025-05-21 20:28:43 +0200

    Impostata navbar

---
+++ src/lib/components/partials/navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+	import NavbarLink from './navbar-link.svelte';
+</script>
+
+<nav class="bg-primary text-primary-foreground flex justify-between">
+	<NavbarLink url="/" testo="Home" />
+
+	<div>
+		<NavbarLink url="/chi-siamo" testo="Chi siamo" />
+		<NavbarLink url="/impatto" testo="Impatto" />
+		<NavbarLink url="/programma" testo="Programma" />
+		<NavbarLink url="/eventi" testo="Eventi" />
+		<NavbarLink url="/unisciti-a-noi" testo="Unisciti a noi!" />
+	</div>
+</nav>
