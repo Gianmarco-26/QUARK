70de302614ad3d89031eb2ecc8a7dc362cbbb5ce
Gianmarco-26 <gianmarcolorusso26@gmail.com>
2025-05-28 18:21:35 +0200

    Aggiunto riferimento {base}

--- src/lib/components/partials/navbar.svelte
+++ src/lib/components/partials/navbar.svelte
@@ -1,15 +1,16 @@
 <script>
 	import NavbarLink from './navbar-link.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <nav class="bg-primary text-primary-foreground flex justify-between">
-	<NavbarLink url="/" testo="Home" />
+	<NavbarLink url="{base}/" testo="Home" />
 
 	<div>
-		<NavbarLink url="/chi-siamo" testo="Chi siamo" />
-		<NavbarLink url="/impatto" testo="Impatto" />
-		<NavbarLink url="/programma" testo="Programma" />
-		<NavbarLink url="/eventi" testo="Eventi" />
-		<NavbarLink url="/unisciti-a-noi" testo="Unisciti a noi!" />
+		<NavbarLink url="{base}/chi-siamo" testo="Chi siamo" />
+		<NavbarLink url="{base}/impatto" testo="Impatto" />
+		<NavbarLink url="{base}/programma" testo="Programma" />
+		<NavbarLink url="{base}/eventi" testo="Eventi" />
+		<NavbarLink url="{base}/unisciti-a-noi" testo="Unisciti a noi!" />
 	</div>
 </nav>
